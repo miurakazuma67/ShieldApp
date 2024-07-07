a369c5442fe0560082ad2c0333317d079ae641ab
三浦一真 <miurakazuma67@gmail.com>
2024-07-07 23:00:03 +0900

    Initial Commit

---
+++ shieldApp/shieldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shieldAppApp.swift
+//  shieldApp
+//
+//  Created by 三浦一真 on 2024/07/07.
+//
+
+import SwiftUI
+
+@main
+struct shieldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shieldAppUITests/shieldAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  shieldAppUITestsLaunchTests.swift
+//  shieldAppUITests
+//
+//  Created by 三浦一真 on 2024/07/07.
+//
+
+import XCTest
+
+final class shieldAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
