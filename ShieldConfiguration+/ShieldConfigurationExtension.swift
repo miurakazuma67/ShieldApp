4c66ae8c2b40a0755e0b17468dac59ca732e002a
三浦一真 <miurakazuma67@gmail.com>
2024-08-24 23:58:12 +0900

    WIP

--- ShieldConfiguration+/ShieldConfigurationExtension.swift
+++ ShieldConfiguration+/ShieldConfigurationExtension.swift
@@ -9,50 +9,16 @@ import ManagedSettings
 import ManagedSettingsUI
 import UIKit
 
-//// Override the functions below to customize the shields used in various situations.
-//// The system provides a default appearance for any methods that your subclass doesn't override.
-//// Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
-//class ShieldConfigurationExtension: ShieldConfigurationDataSource {
-//    override func configuration(shielding application: Application) -> ShieldConfiguration {
-//        // Customize the shield as needed for applications.
-//        ShieldConfiguration()
-//    }
-//    
-//    override func configuration(shielding application: Application, in category: ActivityCategory) -> ShieldConfiguration {
-//        // Customize the shield as needed for applications shielded because of their category.
-//        ShieldConfiguration()
-//    }
-//    
-//    override func configuration(shielding webDomain: WebDomain) -> ShieldConfiguration {
-//        // Customize the shield as needed for web domains.
-//        ShieldConfiguration()
-//    }
-//    
-//    override func configuration(shielding webDomain: WebDomain, in category: ActivityCategory) -> ShieldConfiguration {
-//        // Customize the shield as needed for web domains shielded because of their category.
-//        ShieldConfiguration()
-//    }
-//}
 
 class ShieldConfigurationExtension: ShieldConfigurationDataSource {
     override func configuration(shielding application: Application) -> ShieldConfiguration {
-        return ShieldConfiguration(
-            backgroundBlurStyle: UIBlurEffect.Style.dark,
-            backgroundColor: UIColor.black,
-            icon: UIImage(named: "AppIcon"),
-            title: ShieldConfiguration.Label(
-                text: "アプリ制限中",
-                color: .white
-            ),
-            subtitle: ShieldConfiguration.Label(
-                text: "全ては目標達成のためです。",
-                color: .white
-            ),
-            primaryButtonLabel: ShieldConfiguration.Label(
-                text: "とじる",
-                color: .black
-            ),
-            primaryButtonBackgroundColor: .yellow
+        ShieldConfiguration(
+            icon: UIImage(named: "AppIcon")!,
+            title: .init(text: "Title", color: .white),
+            subtitle: .init(text: "Subtitle", color: .white),
+            primaryButtonLabel: .init(text: "Dismiss", color: .black),
+            primaryButtonBackgroundColor: .blue,
+            secondaryButtonLabel: .init(text: "Unlock", color: .red)
         )
     }
 }
--- shieldApp/Components/Tutorials/TutorialView.swift
+++ shieldApp/Components/Tutorials/TutorialView.swift
@@ -11,20 +11,22 @@ struct TutorialView: View {
     let imageName: String
     let text: String
     var body: some View {
-        VStack(alignment: .center) {
-            Spacer()
-            Text(text)
-            HStack {
-                Image(imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
+        GeometryReader { geometry in
+            VStack(alignment: .center) {
+                Spacer()
+                Text(text)
+                HStack {
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                    Spacer()
+                }
                 Spacer()
             }
-            Spacer()
+            .padding()
+            .background(Color.gray)
+            .edgesIgnoringSafeArea(.all)
         }
-        .padding()
-        .background(Color.gray)
-        .edgesIgnoringSafeArea(.all)
     }
 }
